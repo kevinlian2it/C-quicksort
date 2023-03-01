efdfe8c932e8826bd751350d18bc1f06e3f9527f
Yucheng Lu <yl5163@columbia.edu>
2023-03-01 02:20:48 -0500

    fixed segfault but the result of quicksort is wrong

--- src/quicksort.c
+++ src/quicksort.c
@@ -9,37 +9,38 @@ static int lomuto(void *array, int left, int right, size_t elem_sz,
 static void quicksort_helper(void *array, int left, int right, size_t elem_sz,
                              int (*cmp) (const void*, const void*));
 
-static void swap(void *a, void *b, size_t size) {
-	char *p = a, *q = b, tmp;
-    	for (size_t i = 0; i < size; i++) {
-        	tmp = *(p + i);
-        	*(p + i) = *(q + i);
-        	*(q + i) = tmp;
-    	}
+void swap(void *a, void *b, size_t size) {
+    char temp[size]; 
+    char *p = a, *q = b;
+    for (size_t i = 0; i < size; i++) {
+        temp[i] = *(p + i);
+        *(p + i) = *(q + i);
+        *(q + i) = temp[i];
+    }
 }
 
 static int lomuto(void *array, int left, int right, size_t elem_sz,
                   int (*cmp) (const void*, const void*)) {
-    	char *pivot = (char*) array + right * elem_sz;
-    	int i = left - 1;
-    	for (int j = left; j < right; j++) {
-        	char *elem_j = (char*) array + j * elem_sz;
-        	if (cmp(elem_j, pivot) < 0) {
-            		i++;
-            		swap((char*) array + i * elem_sz, elem_j, elem_sz);
-        	}
-    	}
-    	swap((char*) array + (i+1) * elem_sz, pivot, elem_sz);
-    	return i+1;
+        char *pivot = (char*) array + right * elem_sz;
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+                char *elem_j = (char*) array + j * elem_sz;                
+		if (cmp(elem_j, pivot) <= 0) {
+                        i++;
+                        swap((char*) array + i * elem_sz, elem_j, elem_sz);
+                }
+        }
+        swap((char*) array + (i+1) * elem_sz, pivot, elem_sz);
+        return i+1;
 }
 
 static void quicksort_helper(void *array, int left, int right, size_t elem_sz,
                              int (*cmp) (const void*, const void*)) {
-    	if (left < right) {
-        	int pivot = lomuto(array, left, right, elem_sz, cmp);
-        	quicksort_helper(array, left, pivot-1, elem_sz, cmp);
-        	quicksort_helper(array, pivot+1, right, elem_sz, cmp);
-    	}
+        if (left < right) {
+                int pivot = lomuto(array, left, right, elem_sz, cmp);
+                quicksort_helper(array, left, pivot-1, elem_sz, cmp);
+                quicksort_helper(array, pivot+1, right, elem_sz, cmp);
+        }
 }
 
 int int_cmp(const void *a, const void *b) {
@@ -60,5 +61,8 @@ int str_cmp(const void *a, const void *b) {
     return strcmp(char_a, char_b);
 }
 void quicksort(void *array, size_t len, size_t elem_sz, int (*cmp) (const void*, const void*)) {
+        if (array == NULL) {
+                return;
+        }
 	quicksort_helper(array, 0, (int)len-1, elem_sz, cmp);
 }
--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 #include "quicksort.h"
 
 #define MAX_STRLEN     64 // Not including '\0'
-#define MAX_ELEMENTS 1024 
+#define MAX_ELEMENTS 1024
 
 void display_usage() {
     printf("Usage: ./sort [-i|-d] [filename]\n");
@@ -17,69 +17,70 @@ void display_usage() {
 }
 
 int main(int argc, char** argv) {
-    	int opt;
-    	int flag_int = 0;
-    	int flag_db = 0;
-    	char* filename = NULL;
-    	while ((opt = getopt(argc, argv, ":id")) != -1) {
-        	switch (opt) {
-        	    	case 'i':
-                		flag_int = 1;
-                		break;
-            		case 'd':
-                		flag_db = 1;
-                		break;
-            		case '?':
-                		fprintf(stderr, "Error: Unknown option '-%c' received.\n", optopt);
-                		display_usage();
-                		return EXIT_FAILURE;
-            		default:
-                		display_usage();
-                		return EXIT_FAILURE;
-        	}
-    	}
-	if ((argc - optind) > 1) {
-		fprintf(stderr, "Error: Too many files specified.\n");
-		return EXIT_FAILURE;
-	}
-   	if (optind == argc-1) {
-        	filename = argv[optind];
-    	}
-	FILE* input = stdin;	
-    	if(filename) {
-		input = fopen(filename, "r");
-		if(input == NULL) {
-			fprintf(stderr, "Error: Cannot open '%s'. No such file or directory.\n", filename);
-			return EXIT_FAILURE;
-		}
-	}
-
-	char input_arr[MAX_ELEMENTS][MAX_STRLEN];
-	int num_elements = 0;
-	while (num_elements < MAX_ELEMENTS && fgets(input_arr[num_elements], MAX_STRLEN, input)) {
-		char* newline = strchr(input_arr[num_elements], '\n');
-		if(newline) {
-			*newline = '\0';
-		}
-		num_elements++;
-	}
-	if(flag_int + flag_db == 2) {
-                fprintf(stderr, "Error: Too many flags specified.\n");
+        int opt;
+        int flag_int = 0;
+        int flag_db = 0;
+        char* filename = NULL;
+        while ((opt = getopt(argc, argv, ":id")) != -1) {
+                switch (opt) {
+                        case 'i':
+                                flag_int = 1;
+                                break;
+                        case 'd':
+                                flag_db = 1;
+                                break;
+                        case '?':
+                                fprintf(stderr, "Error: Unknown option '-%c' received.\n", optopt);
+                                display_usage();
+                                return EXIT_FAILURE;
+                        default:
+                                display_usage();
+                                return EXIT_FAILURE;
+                }        }
+        if ((argc - optind) > 1) {
+                fprintf(stderr, "Error: Too many files specified.\n");
                 return EXIT_FAILURE;
         }
+        if (optind == argc-1) {
+                filename = argv[optind];
+        }
+        FILE* input = stdin;
+        if(filename) {
+                input = fopen(filename, "r");
+                if(input == NULL) {
+                        fprintf(stderr, "Error: Cannot open '%s'. No such file or directory.\n", filename);
+                        return EXIT_FAILURE;
+                }
+        }
+
+        char input_arr[MAX_ELEMENTS][MAX_STRLEN];
+        int num_elements = 0;
+        while (num_elements < MAX_ELEMENTS && fgets(input_arr[num_elements], MAX_STRLEN, input)) {
+ 	 	  char* newline = strchr(input_arr[num_elements], '\n');
+ 	 	  if(newline) {
+ 	       		*newline = '\0';
+    	   	  }
+    	   	  else {
+        		input_arr[num_elements][MAX_STRLEN-1] = '\0';
+        		while(fgetc(input) != '\n' && !feof(input)); // consume remaining characters in the line
+    		  }
+    		  num_elements++;
+	}
+        
 	if(flag_int) {
-		quicksort(input_arr,sizeof(input_arr),sizeof(int),int_cmp);
+		quicksort((void *) input_arr, num_elements, sizeof(input_arr[0]), int_cmp);                		
+        }
+        else if(flag_db) {
+                quicksort((void *) input_arr, num_elements, sizeof(input_arr[0]), dbl_cmp);
 	}
-	else if(flag_db) {
-		quicksort(input_arr,sizeof(input_arr),sizeof(double),dbl_cmp);
+        else {
+		quicksort((void *) input_arr, num_elements, sizeof(input_arr[0]), str_cmp);                
 	}
-	else {
-		quicksort(input_arr,sizeof(input_arr),sizeof(char*),str_cmp);		}
 	for(int i = 0;i < num_elements; i++) {
-		printf("%s\n", input_arr[i]);
-	}
-	if(filename) {
-		fclose(input);
-	}
-    	return EXIT_SUCCESS;
-}
+                printf("%s\n", input_arr[i]);
+        }
+        if(filename) {
+                fclose(input);
+        }
+        return EXIT_SUCCESS;
+} 
